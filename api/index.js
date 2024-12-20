b9c1bbe2835cf63ff40c6d9bbe6e1066b119675a
Opeyemi <astro8va@gmail.com>
2024-12-20 08:29:48 +0100

    added

--- api/index.js
+++ api/index.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const app = express();
 const port = 3001; // Example port
 
-app.use(cors()); // Enable CORS
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Hello from the server!' });
